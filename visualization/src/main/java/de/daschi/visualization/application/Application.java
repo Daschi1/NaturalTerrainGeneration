4c56daaa41f75e15b66f3edc1e98727943a84095
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-05-14 22:55:05 +0200

    Added canvas tp Application.java

--- visualization/src/main/java/de/daschi/visualization/application/Application.java
+++ visualization/src/main/java/de/daschi/visualization/application/Application.java
@@ -1,15 +1,31 @@
 package de.daschi.visualization.application;
 
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
+
+    private static Canvas canvas; //main canvas
 
     public static void main(final String[] args) {
         Application.launch(args); //launch application
     }
 
+    public static Canvas getCanvas() {
+        return Application.canvas;
+    }
+
     @Override
     public void start(final Stage primaryStage) {
+        primaryStage.setTitle("NaturalTerrainGeneration - Visualization"); //set title
+        primaryStage.setResizable(false); //set resizable
 
+        Application.canvas = new Canvas(); //create canvas
+        final BorderPane root = new BorderPane(Application.canvas); //create root
+        primaryStage.setScene(new Scene(root, 500, 500)); //create scene
+
+        primaryStage.show(); //show primaryStage
     }
 }
