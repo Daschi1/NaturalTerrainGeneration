9691b9343fdcc02081e6a7d7bfe1327b41f0e7fe
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-05-14 22:33:27 +0200

    Added comments

--- visualization/src/main/java/de/daschi/visualization/application/Application.java
+++ visualization/src/main/java/de/daschi/visualization/application/Application.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 public class Application extends javafx.application.Application {
 
     public static void main(final String[] args) {
-        Application.launch(args);
+        Application.launch(args); //launch application
     }
 
     @Override
